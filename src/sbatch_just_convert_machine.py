6dd2a1b8f3d963d775b5737a2604738a4a37c004
robertej19 <robertej@mit.edu>
2023-01-04 00:15:08 -0500

    typo causing a bug, trying to fix

--- src/sbatch_just_convert_machine.py
+++ src/sbatch_just_convert_machine.py
@@ -69,7 +69,7 @@ mv {2} {1}FC_Files/{4}_filt_conv.root
 """.format( args.convert_dir, 
     args.return_dir,
     output_name, 
-    file_sub_string, 
+    file_sub_string.split(".")[0]+".hipo", #strange workaround for whitespace bug
     file_sub_string.split("/")[-1].split(".")[0])  
     outfile.write(header+string)
     outfile.close()
